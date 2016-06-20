f3db4e5f8bff8dd2dbde0ba00849838f9839da37
Jefftree <jeffrey.ying86@live.com>
2016-06-20 13:16:47 -0400

    symlink and diff changes

--- cron.sh
+++ cron.sh
@@ -1,6 +1,16 @@
 CURDIR=$(dirname $(type -p $0 | grep -oE '[^ ]+$' | head -1))
 
-for file in $(ls $CURDIR/*.md); do
-    notes file pdf
+git pull > /dev/null
+git diff HEAD~ --name-only
+
+for file in $(ls); do
+    if [[ $file == *.md ]]; then
+        echo $file
+        stripext=${tmp%.*}
+        if [ ! -f $stripext.pdf ]; then
+            notes file pdf
+        fi
+    fi
 done
+
 
